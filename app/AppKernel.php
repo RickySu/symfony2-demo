1066bb89db396d1e0c30995655c3a6e484c113d7
Ricky Su <ricky@ez2.us>
2013-07-03 22:28:02 +0800

    add propel bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Ricky\Bundle\TestBundle\RickyTestBundle(),
+            new Propel\PropelBundle\PropelBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
